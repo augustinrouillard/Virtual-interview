ee18f544181a72f8085144969a558e8e8cc5b246
Augustin Rouillard <120126872+augustinrouillard@users.noreply.github.com>
2026-02-05 15:17:11 +0100

    feat/apple-basics

---
+++ Virtual interview/Virtual interview/Virtual_interviewApp.swift
@@ -0,0 +1,33 @@
+//
+//  Virtual_interviewApp.swift
+//  Virtual interview
+//
+//  Created by bpce-si on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Virtual_interviewApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
